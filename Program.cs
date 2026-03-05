6d5fb9c85ef122fbc36e49a2fe5e2e334d073393
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2026-03-04 23:04:04 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿//Imagen 6
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaterialFactory fabrica;
+            fabrica = new MaterialPresencialFactory();
+
+            Guia guia = fabrica.CrearGuia();
+            Examen examen = fabrica.CrearExamen();
+
+            guia.Mostrar();
+            examen.Aplicar();
+
+            Console.WriteLine("");
+
+            fabrica = new MaterialVirtualFactory();
+
+            guia = fabrica.CrearGuia();
+            examen = fabrica.CrearExamen();
+
+            guia.Mostrar();
+            examen.Aplicar();
+
+            Console.WriteLine("");
+
+            //Hibrido
+            fabrica = new MaterialHibridoFactory();
+
+            guia = fabrica.CrearGuia();
+            examen = fabrica.CrearExamen();
+
+            guia.Mostrar();
+            examen.Aplicar();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//Imagen 1
+namespace Abstract_Factory
+{
+    public abstract class MaterialFactory
+    {
+        public abstract Guia CrearGuia();
+        public abstract Examen CrearExamen();
+    }
+
+
+    public abstract class Guia
+    {
+        public abstract void Mostrar();
+    }
+
+    public abstract class Examen
+    {
+        public abstract void Aplicar();
+    }
+
+}
+
+//Imagen 2
+namespace Abstract_Factory
+{
+    public class GuiaImpresa : Guia
+    {
+        public override void Mostrar()
+        {
+            Console.WriteLine("Mostrando la guia impresa");
+        }
+    }
+
+    public class ExamenEnPapel : Examen
+    {
+        public override void Aplicar()
+        {
+            Console.WriteLine("Se aplica examen en papel");
+        }
+    }
+}
+
+//Imagen3
+namespace Abstract_Factory
+{
+    public class GuiaPDF : Guia
+    {
+        public override void Mostrar()
+        {
+            Console.WriteLine("Mostrando la guia PDF");
+        }
+    }
+
+    public class ExamenOnline : Examen
+    {
+        public override void Aplicar()
+        {
+            Console.WriteLine("Se aplica examen en linea");
+        }
+    }
+}
+
+//Implementacion Hibrida
+namespace Abstract_Factory
+{
+    public class GuiaHibrida : Guia
+    {
+        public override void Mostrar()
+        {
+            Console.WriteLine("Guia en Modalidad Hibrida (semipresencial)");
+        }
+    }
+
+    public class ExamenMixto : Examen
+    {
+        public override void Aplicar()
+        {
+            Console.WriteLine("Se aplica Examen Mixto");
+        }
+    }
+}
+
+//Imagen4
+namespace Abstract_Factory
+{
+    public class MaterialPresencialFactory : MaterialFactory
+    {
+        public override Guia CrearGuia()
+        {
+            return new GuiaImpresa();
+        }
+    
+        public override Examen CrearExamen()
+        {
+            return new ExamenEnPapel();
+        }
+    }
+}
+
+//Imagen5
+namespace Abstract_Factory
+{
+    public class MaterialVirtualFactory : MaterialFactory
+    {
+        public override Guia CrearGuia()
+        {
+            return new GuiaPDF();
+        }
+    
+        public override Examen CrearExamen()
+        {
+            return new ExamenOnline();
+        }
+    }
+}
+
+//Implementacion Hibrida
+namespace Abstract_Factory
+{
+    public class MaterialHibridoFactory : MaterialFactory
+    {
+        public override Guia CrearGuia()
+        {
+            return new GuiaHibrida();
+        }
+    
+        public override Examen CrearExamen()
+        {
+            return new ExamenMixto();
+        }
+    }
+}
+
